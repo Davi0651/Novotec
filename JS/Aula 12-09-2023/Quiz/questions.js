46800f4c3fdad688be46a48a137913b9471cf47f
Davi0651 <davioficial065@gmail.com>
2023-09-12 12:24:14 -0300

    120923

---
+++ JS/Aula 12-09-2023/Quiz/questions.js
@@ -0,0 +1,18 @@
+export default [
+    {
+      question: "Qual o jogo mais famoso atualmente ?",
+      answers: [
+        { option: "Minecraft", correct: true },
+        { option: "Fortnite", correct: false },
+        { option: "Call of Duty", correct: false },
+      ],
+    },
+    {
+      question: "Qual o jogo mais vendido do mundo ?",
+      answers: [
+        { option: "GTA 5", correct: false },
+        { option: "Minecraft", correct: true },
+        { option: "Super Mario Bros", correct: false },
+      ],
+    }, 
+  ];
